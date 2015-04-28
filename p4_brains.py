11a66b486542efef29e613e4bc045c6cefdfb779
Mark Biundo <mdbiundo@ucsc.edu>
2015-04-27 17:35:31 -0700

    state changes, attack implemented

--- p4_brains.py
+++ p4_brains.py
@@ -50,7 +50,18 @@ class SlugBrain:
 		self.state = 'idle'
 		self.target = None
 
-
+	def followmantis(self):
+		try:
+			mantis = self.body.find_nearest('Mantis')
+			self.body.follow(mantis)
+			self.state = 'attack'
+			self.body.set_alarm(2)
+		except ValueError:
+			print "No more mantises!"
+			self.body.stop()
+			self.state = 'idle'
+		
+		
 	def handle_event(self, message, details):
 		# TODO: IMPLEMENT THIS METHOD
 		#  (Use helper methods and classes to keep your code organized where
@@ -58,11 +69,44 @@ class SlugBrain:
 		
 		#print "Mess: " + str(message) + " Details: " + str(details)
 		
-		if self.state is 'idle':
+		if message == 'order':
+			if details == 'i':
+				#go idle
+				self.body.stop()
+				self.state = 'idle'
+			elif details == 'a':
+				#switch to attack mode if there is a mantis to attack
+				self.followmantis()
+			elif details == 'h':
+				#switch to harvest mode
+				self.state = 'harvest'
+			elif details == 'b':
+				#switch to build mode
+				self.state = 'build'
+			else:
+				#goto target, then idle
+				try:
+					self.body.go_to(details)
+					self.state = 'idle'
+				except TypeError:
+					print "Invalid command: " + str(details)
+			#print self.state
 		
-			if message == 'order':
-				self.body.go_to(details)
- 
+		if self.state == 'attack':
+			if message == 'timer':
+				self.followmantis()
+				
+			if message == 'collide' and details['what'] == 'Mantis':
+				mantis = details['who']
+				mantis.amount -= .05
+					
+		
+		elif self.state == 'harvest':
+			pass
+		
+		elif self.state == 'build':
+			pass
+		
 
 world_specification = {
   'worldgen_seed': 13, # comment-out to randomize
